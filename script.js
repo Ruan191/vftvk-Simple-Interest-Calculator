fd638a1e410d41ea65737f0ed381d48cd55fb5f3
Ruan191 <theredsting@gmail.com>
2022-01-20 13:45:07 +0200

    final changes

--- script.js
+++ script.js
@@ -15,17 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function compute()
     {
-        let resultArea = document.getElementById("result");
         let amount = Number(amountTextBox.value);
-        let years = Number(numberOfYearsSelector.value);
-        let rate = Number(interestRateBar.value);
-        let date = new Date();
 
-        resultArea.innerHTML = "If you deposit " + amount + 
-        "<br>at an interest rate of " + rate + 
-        "%.<br>You will receive an amount of " + 
-        Math.round(((amount * (1 + ((rate / 100)) * years) - amount)), 1)
-        + "<br>in the year " + (date.getFullYear() + years);
+        if (amount <= 0){
+            alert("Please enter a positive value")
+            amountTextBox.focus();
+        }else{
+            const y = "<toYellow>";
+            const yc = "</toYellow>";
+
+            let resultArea = document.getElementById("result");
+            let years = Number(numberOfYearsSelector.value);
+            let rate = Number(interestRateBar.value);
+            let date = new Date();
+    
+            resultArea.innerHTML = `If you deposit ${y}` + amount + 
+            `${yc}<br>at an interest rate of ${y}` + rate + 
+            `%${yc}.<br>You will receive an amount of ${y}` + 
+            Math.round(((amount * (1 + ((rate / 100)) * years) - amount)), 1)
+            + `${yc}<br>in the year ${y}` + (date.getFullYear() + years) + yc;
+        }
     }
 
 })
